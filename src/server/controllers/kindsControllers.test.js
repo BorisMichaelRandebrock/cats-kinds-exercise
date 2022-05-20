fa73ff2c916bf0d18516127f42e007b0d90238a6
Boris <boris@randebrock.com>
2022-05-20 13:50:25 +0200

    added workflows

--- src/server/controllers/kindsControllers.test.js
+++ src/server/controllers/kindsControllers.test.js
@@ -4,7 +4,12 @@ const listKinds = require("./kindsControllers");
 describe("Given the kindsController Function", () => {
   describe("When invoked with a Kind-List request", () => {
     test("Then it should respond invoking the json method with an object with the property kind: Gatoperro", async () => {
-      listKinds(req, res);
+      const res = {
+        status: jest.fn().mockReturnThis,
+        json: jest.fn,
+      };
+
+      listKinds(null, res);
     });
   });
 });
